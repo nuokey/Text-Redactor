d4d8e01b6619ce18d43da810fbbe79bd8a4fbc35
Timo <66961498+nuokey@users.noreply.github.com>
2023-02-02 22:35:30 +0300

    Update

--- script.js
+++ script.js
@@ -4,69 +4,37 @@ textField = document.getElementById("text-field");
 
 let cursorCoord = 0;
 
+let lastText = "";
+let text;
+
 function importFile(input) {
   let file = input.files[0];
+
+  let htmlText = "";
 
   let reader = new FileReader();
 
   reader.readAsText(file);
 
   reader.onload = function() {
-    let text = reader.result;
+    text = reader.result;
+
+    htmlText.replace(" ", "&nbsp");
+
+    text = text.split("\r\n");
+
+    console.log(text);
+    
+    text.forEach(element => {
+        htmlText += `<div>${element}</div>`;
+    });
+
+    // htmlText.replace(" ", "&nbsp");
+
+    textField.innerHTML = htmlText;
+
     
 
-    textField.innerHTML = textToHTML(text);
+    console.log(textField.textContent);
   };
-}
-
-document.addEventListener("keyup", function() {
-  console.log("file changed");
-
-  text = textField.textContent;
-  console.log(text);
-
-  let cursor = document.getSelection();
-
-  console.log(cursor);
-
-  textField.innerHTML = textToHTML(text);
-
-  textField.setSelectionRange(10, 10);
-  
-})
-
-function colorCheck(word) {
-  let color = "white";
-  if (word == "word") {
-    color = "red";
-  }
-
-  return color
-}
-
-function textToHTML(text) {
-  let htmlText = "";
-
-  let word = "";
-
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] == " ") {
-      htmlText += `<span class="word" style="color: ${colorCheck(word)}">${word}</span>&nbsp`;
-      word = "";
-    }
-    else if (text[i] == ".") {
-      htmlText += `<span class="word" style="color: ${colorCheck(word)}">${word}</span><span class="word" style="color: ${colorCheck(word)}">.</span>`;
-      word = "";
-    }
-    else if (text[i] == "\n") {
-      htmlText += `<span class="word" style="color: ${colorCheck(word)}">${word}\n</span><br>`;
-      word = "";
-    }
-    else {
-      word += text[i];
-    }
-  }
-  htmlText += `<span class="word" style="color: ${colorCheck(word)}">${word}</span>`;
-
-  return htmlText
 }
